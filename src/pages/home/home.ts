b79f552d713223c51e2b12f23455a048df5cb248
Szasz Barnabas <barna@hudeb669235.ni.corp.natinst.com>
2017-12-28 14:53:35 +0100

    proxy added - works online

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private proxy = 'https://cors-anywhere.herokuapp.com/'
+  private items  = [];
+  private urls = [
+    'https://www.teletrader.com/fidelity-fd-glob-technology-a-acc-usd/funds/details/FU_100049764',
+    'https://www.teletrader.com/jpm-us-technology-a-acc-usd/funds/details/FU_100010194',
+    'https://www.teletrader.com/pioneer-fd-u-s-fund-gro-a-nd-usd/funds/details/FU_100024783'
+  ]
 
+  constructor(public navCtrl: NavController, public http: Http) {
+    for (let url in this.urls) {
+      this.http.get(this.proxy+this.urls[url]).subscribe(data => {
+      var $parsedResults = $.parseHTML(data.text());
+      let currentValue = $($parsedResults).find( "#Heading div.component.security-details div table tbody tr td.cell-first.last span span.last" ).text();
+      let name = $($parsedResults).find( "#Heading div.heading-holder.clearfix h1" ).text();
+      this.items.push({ currentValue, name });
+    });
+    }
   }
 
 }
