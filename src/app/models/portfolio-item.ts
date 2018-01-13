0899d435ed024dfc1bb2b1de0ea9a816b429e7ce
Szasz Barnabas <barna@Szasz-MacBook-Pro.local>
2018-01-13 22:45:44 +0100

    external JSON config

--- src/app/models/portfolio-item.ts
+++ src/app/models/portfolio-item.ts
@@ -2,23 +2,33 @@ import { SourcePage } from "./source-page";
 import * as $ from 'jquery';
 
 export class PortfolioItem {
-    
 
-        public name: string
-    public currentValue: number 
 
-       constructor(public url: string, public type: SourcePage, public originalValue: number,public originalAmount: number){
-    
-       }
+    public name: string
+    public currentValue: number
+    public originalValue: number
+    public originalAmount: number
 
-       public profit() {
-           if (isNaN(this.currentValue)||isNaN(this.originalAmount)||isNaN(this.originalValue) )return 0;
-           return Math.round(this.originalAmount*(this.currentValue - this.originalValue) * 100) / 100
-       }
-
-       public refresh(data) {
-          var $parsedResults = $.parseHTML(data.text());
-          this.currentValue = $($parsedResults).find(this.type.valuePath).text();
-          this.name = $($parsedResults).find(this.type.namePath).text();
-        }
+    constructor(public url: string, public type: SourcePage) {
+        
     }
+    
+    public setOriginalValue(value){
+        this.originalValue = value
+    }
+    public setOriginalAmount(amount) {
+        this.originalAmount = amount
+    }
+
+    public profit() {
+        if (isNaN(this.currentValue) || isNaN(this.originalAmount) || isNaN(this.originalValue)) return 0;
+        return Math.round(this.originalAmount * (this.currentValue - this.originalValue) * 100) / 100
+    }
+
+    public refresh(data) {
+        var $parsedResults = $.parseHTML(data.text());
+        this.currentValue = $($parsedResults).find(this.type.valuePath).text();
+        this.name = $($parsedResults).find(this.type.namePath).text();
+    }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,27 +13,27 @@ import { Http } from '@angular/http';
 })
 export class HomePage {
 
+  private items = []
   private pageTypes = [];
 
-  private proxy = 'https://cors-anywhere.herokuapp.com/'  
+  private proxy = 'https://cors-anywhere.herokuapp.com/';
 
-  private items = []
-  
-
-  constructor(public navCtrl: NavController, public http:Http, private storage: Storage, public alertCtrl: AlertController) {
-    this.items.forEach( item  => {
-      this.getItem(item)
+  constructor(public navCtrl: NavController, private storage: Storage, public http: Http, public alertCtrl: AlertController) {
+    this.storage.get("settings").then((val) => {
+      this.loadSettings(val);
+      this.items.forEach(item => {
+        this.getItem(item)
+      });
     });
   }
 
-  getItem(item)
-  {
+  getItem(item) {
     this.http.get(this.proxy + item.url)
-    .subscribe(data => {
-      item.refresh(data);
-    }, (error: any) => {
-      console.log("error downloading a page",item.url, error)
-    });
+      .subscribe(data => {
+        item.refresh(data);
+      }, (error: any) => {
+        console.log("error downloading a page", item.url, error)
+      });
 
   }
 
@@ -45,11 +45,11 @@ export class HomePage {
         name: 'url',
         placeholder: 'http://',
         type: 'url'
-      },{
+      }, {
         name: 'originalValue',
         placeholder: 'original value',
         type: 'number'
-      },{
+      }, {
         name: 'originalAmount',
         placeholder: 'amount',
         type: 'number'
@@ -61,10 +61,12 @@ export class HomePage {
         {
           text: 'Save',
           handler: data => {
-            let item = new PortfolioItem(data.url,this.pageTypes[0], data.originalValue, data.originalAmount)
+            let item = new PortfolioItem(data.url, this.pageTypes[0]);
+            item.setOriginalValue(data.originalValue);
+            item.setOriginalAmount(data.originalAmount);
             this.getItem(item);
             this.items.push(item);
-            console.log(item);            
+            console.log(item);
           }
         }
       ]
@@ -81,17 +83,17 @@ export class HomePage {
         placeholder: 'http://',
         type: 'url',
         value: item.url
-      },{
+      }, {
         name: 'originalValue',
         placeholder: 'original value',
         type: 'number',
         value: item.originalValue
-      },{
+      }, {
         name: 'originalAmount',
         placeholder: 'amount',
         type: 'number',
         value: item.originalAmount
-       }],
+      }],
       buttons: [
         {
           text: 'Cancel'
@@ -100,12 +102,14 @@ export class HomePage {
           text: 'Save',
           handler: data => {
             let index = this.items.indexOf(item);
-            let newItem = new PortfolioItem(data.url,item.type, data.originalValue, data.originalAmount)
+            let newItem = new PortfolioItem(data.url, item.type);
+            newItem.setOriginalValue(item.originalValue);
+            newItem.setOriginalAmount(item.originalAmount);
             this.getItem(newItem);
             if (index > -1) {
               this.items[index] = newItem;
             }
-            console.log(item);            
+            console.log(item);
           }
         }
       ]
@@ -123,47 +127,54 @@ export class HomePage {
   }
 
   settings() {
-    
-        let prompt = this.alertCtrl.create({
-          title: 'Load JSON settings',
-          subTitle: 'You can use https://jsonblob.com to create a portfolio descriptor JSON.',
-          inputs: [{
-            name: 'url',
-            placeholder: 'http://',
-            type: 'url'
-          }],
-          buttons: [
-            {
-              text: 'Cancel'
-            },
-            {
-              text: 'Save',
-              handler: data => {
-                this.http.get(data.url)
-                .subscribe(data => {
-                  let json = data.json();
-                  console.log(json);
-                  this.pageTypes = [];
-                  for (var i of json.pageTypes) {
-                    let newType = new SourcePage(i.site,i.name, i.value);
-                    console.log(newType);
-                    this.pageTypes.push(newType);
-                  }
-                  this.items = [];
-                  for (var i of json.items) {
-                    let newItem = new PortfolioItem(i.url,this.pageTypes[i.pageType], i.originalValue, i.originalAmount)
-                    console.log(newItem);
-                    this.items.push(newItem);
-                    this.getItem(newItem);
-                  }
-                }, (error: any) => {
-                  console.log("error downloading settings", error)
-                });
-                      
-              }
-            }
-          ]
-        });
-        prompt.present();
-      }
+
+    let prompt = this.alertCtrl.create({
+      title: 'Load JSON settings',
+      subTitle: 'You can use https://jsonblob.com to create a portfolio descriptor JSON.',
+      inputs: [{
+        name: 'url',
+        placeholder: 'http://',
+        type: 'url'
+      }],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.storage.set("settings", data.url)
+            this.loadSettings(data.url);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  private loadSettings(url) {
+    this.http.get(url)
+      .subscribe(data => {
+        let json = data.json();
+        console.log(json);
+        this.pageTypes = [];
+        for (var i of json.pageTypes) {
+          let newType = new SourcePage(i.site, i.name, i.value);
+          console.log(newType);
+          this.pageTypes.push(newType);
+        }
+        this.items = [];
+        for (var i of json.items) {
+          let newItem = new PortfolioItem(i.url, this.pageTypes[i.pageType]);
+          newItem.setOriginalValue(i.originalValue);
+          newItem.setOriginalAmount(i.originalAmount);
+          console.log(newItem);
+          this.items.push(newItem);
+          this.getItem(newItem);
+        }
+      }, (error: any) => {
+        console.log("error downloading settings", error)
+      });
+
+  }
 }
